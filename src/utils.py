ddeaa47f0358c44cec1e0db98edf8bf2a1e9b227
fahimulkabir <hamimkabir14@gmail.com>
2026-01-23 02:01:05 -0600

    Data loading & Evaluation

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def prepare_tensors(norm_matrix, embeddings, device):
     """Converts numpy arrays to GPU tensors."""
@@ -24,3 +25,16 @@ def contact_to_distance(contacts, alpha):
     # Normalize max to 1.0
     d = d / torch.max(torch.nan_to_num(d, posinf=0))
     return d
+
+def write_pdb(positions, pdb_file):
+    """Saves the 3D coordinates to a .pdb file."""
+    # Scale positions up for better visualization (standard HiC-GNN scaling)
+    positions = positions * 100 
+    
+    with open(pdb_file, "w") as o_file:
+        o_file.write("\n")
+        bin_num = len(positions)
+        for i in range(1, bin_num+1):
+            line = "ATOM  %5d  CA  MET A%4d    %8.3f%8.3f%8.3f  1.00  0.00           C\n" % (i, i, positions[i-1][0], positions[i-1][1], positions[i-1][2])
+            o_file.write(line)
+        o_file.write("END")
